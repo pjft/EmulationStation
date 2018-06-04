4b836879022d1768bf6eb5aa62a88c7f0b722d7f
pjft <paulo.j.tavares@gmail.com>
2018-06-04 14:49:50 +0100

    Test

--- es-core/src/components/ImageComponent.cpp
+++ es-core/src/components/ImageComponent.cpp
@@ -343,9 +343,9 @@ void ImageComponent::render(const Transform4x4f& parentTrans)
 			glEnableClientState(GL_VERTEX_ARRAY);
 			glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 			glEnableClientState(GL_COLOR_ARRAY);
-			LOG(LogError) << "Path: " << mDefaultPath << "Vertex: " << mVertices[0].pos[0] << " : " <<
+			/*LOG(LogError) << "Path: " << mDefaultPath << "Vertex: " << mVertices[0].pos[0] << " : " <<
 				 mVertices[0].pos[1] << " -- Texture: " << mVertices[0].tex[0] << " : " <<
-				 mVertices[0].tex[1];
+				 mVertices[0].tex[1];*/
 			glVertexPointer(2, GL_FLOAT, sizeof(Vertex), &mVertices[0].pos);
 			glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), &mVertices[0].tex);
 			glColorPointer(4, GL_UNSIGNED_BYTE, 0, mColors);
